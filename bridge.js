9d290c4d00fd0e2cd87ba9275149aa22c3df543a
thecardrogue <54993671+thecardrogue@users.noreply.github.com>
2020-01-23 10:31:00 -0500

    unfinished adding the railing

--- bridge.js
+++ bridge.js
@@ -1,12 +1,14 @@
 //creates the bridge
-function bridge(x, y, xS, yS) {
+function bridge(x, y, xS, yS, img) {
   this.x = x;
   this.y = y;
   this.xS = xS;
   this.yS = yS;
-
+  this.pic = img;
+ 
+  image(this.pic);
   this.display = function() {
-    fill(150, 255, 50);
+    fill(101, 67, 33);
     rect(this.x, this.y, this.xS, this.yS);
   };
 
