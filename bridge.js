febdaacfbd4f8df935d1332c6daa711cdf2c0fa2
thecardrogue <54993671+thecardrogue@users.noreply.github.com>
2020-01-23 09:32:01 -0500

    made the file

---
+++ bridge.js
@@ -0,0 +1,13 @@
+//creates the bridge
+function bridge(x, y, xS, yS) {
+  this.x = x;
+  this.y = y;
+  this.xS = xS;
+  this.yS = yS;
+
+  this.display = function() {
+    fill(150, 255, 50);
+    rect(this.x, this.y, this.xS, this.yS);
+  };
+
+}
