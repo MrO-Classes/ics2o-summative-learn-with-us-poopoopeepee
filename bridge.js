c4b47ca1ebb6e31088526c459e14a794bdf3fe20
thecardrogue <54993671+thecardrogue@users.noreply.github.com>
2020-01-24 10:46:14 -0500

    removed the rail

--- bridge.js
+++ bridge.js
@@ -1,12 +1,11 @@
 //creates the bridge
-function bridge(x, y, xS, yS, img) {
+function bridge(x, y, xS, yS, ) {
   this.x = x;
   this.y = y;
   this.xS = xS;
   this.yS = yS;
-  this.pic = img;
  
-  image(this.pic);
+ 
   this.display = function() {
     fill(101, 67, 33);
     rect(this.x, this.y, this.xS, this.yS);
